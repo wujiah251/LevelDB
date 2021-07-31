4797b0b211bc832bcd8fb79e5aa9101b89201ae8
wujiahao <wujiahao.mail@gmail.com>
2021-07-31 17:38:34 +0800

    学习memtable

--- leveldb-master/db/memtable.h
+++ leveldb-master/db/memtable.h
@@ -1,7 +1,3 @@
-// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file. See the AUTHORS file for names of contributors.
-
 #ifndef STORAGE_LEVELDB_DB_MEMTABLE_H_
 #define STORAGE_LEVELDB_DB_MEMTABLE_H_
 
@@ -11,90 +7,82 @@
 #include "db/skiplist.h"
 #include "util/arena.h"
 
-namespace leveldb {
+namespace leveldb
+{
 
-class InternalKeyComparator;
-class Mutex;
-class MemTableIterator;
+  class InternalKeyComparator;
+  class Mutex;
+  class MemTableIterator;
 
-class MemTable {
- public:
-  // MemTables are reference counted.  The initial reference count
-  // is zero and the caller must call Ref() at least once.
-  explicit MemTable(const InternalKeyComparator& comparator);
+  class MemTable
+  {
+  public:
+    // MemTables are reference counted.  The initial reference count
+    // is zero and the caller must call Ref() at least once.
+    explicit MemTable(const InternalKeyComparator &comparator);
 
-  // Increase reference count.
-  void Ref() { ++refs_; }
+    // Increase reference count.
+    void Ref() { ++refs_; }
 
-  // Drop reference count.  Delete if no more references exist.
-  void Unref() {
-    --refs_;
-    assert(refs_ >= 0);
-    if (refs_ <= 0) {
-      delete this;
+    // Drop reference count.  Delete if no more references exist.
+    void Unref()
+    {
+      --refs_;
+      assert(refs_ >= 0);
+      if (refs_ <= 0)
+      {
+        delete this;
+      }
     }
-  }
 
-  // Returns an estimate of the number of bytes of data in use by this
-  // data structure. It is safe to call when MemTable is being modified.
-  size_t ApproximateMemoryUsage();
+    // 返回memtable实例的内存使用量
+    size_t ApproximateMemoryUsage();
 
-  // Return an iterator that yields the contents of the memtable.
-  //
-  // The caller must ensure that the underlying MemTable remains live
-  // while the returned iterator is live.  The keys returned by this
-  // iterator are internal keys encoded by AppendInternalKey in the
-  // db/format.{h,cc} module.
-  Iterator* NewIterator();
+    // 创建一个迭代器，其实就是一个MemTableIterator类实例。
+    // 内部封装了skiplist
+    Iterator *NewIterator();
+    // 添加一个key-value
+    void Add(SequenceNumber seq, ValueType type,
+             const Slice &key,
+             const Slice &value);
+    // 根据key获取value
+    bool Get(const LookupKey &key, std::string *value, Status *s);
 
-  // Add an entry into memtable that maps key to value at the
-  // specified sequence number and with the specified type.
-  // Typically value will be empty if type==kTypeDeletion.
-  void Add(SequenceNumber seq, ValueType type,
-           const Slice& key,
-           const Slice& value);
+  private:
+    ~MemTable(); // Private since only Unref() should be used to delete it
 
-  // If memtable contains a value for key, store it in *value and return true.
-  // If memtable contains a deletion for key, store a NotFound() error
-  // in *status and return true.
-  // Else, return false.
-  bool Get(const LookupKey& key, std::string* value, Status* s);
+    // MemTable类内部使用的键比较器类型。
+    struct KeyComparator
+    {
+      const InternalKeyComparator comparator;
+      explicit KeyComparator(const InternalKeyComparator &c) : comparator(c) {}
+      int operator()(const char *a, const char *b) const;
+    };
 
- private:
-  ~MemTable();  // Private since only Unref() should be used to delete it
+    friend class MemTableIterator;
+    friend class MemTableBackwardIterator;
 
-  // MemTable类内部使用的键比较器类型。
-  struct KeyComparator {
-    const InternalKeyComparator comparator;
-    explicit KeyComparator(const InternalKeyComparator& c) : comparator(c) { }
-    int operator()(const char* a, const char* b) const;
+    // 定义一个MemTable用来存储元素的skiplist类型，该skiplist内部的key类型是字符串，
+    // 所用的key比较器类型是KeyComparator
+    typedef SkipList<const char *, KeyComparator> Table;
+
+    // MemTable类实例用来比较内部元素大小的比较器。
+    KeyComparator comparator_;
+
+    // MemTable类实例的内部引用计数器。
+    int refs_;
+
+    // MemTable类实例用来给元素分配内存时使用的内存管理器。
+    Arena arena_;
+
+    // MemTable类实例用来存储元素的表，其内部实现其实是一个跳表，skiplist。
+    Table table_;
+
+    // No copying allowed
+    MemTable(const MemTable &);
+    void operator=(const MemTable &);
   };
-  
-  friend class MemTableIterator;
-  friend class MemTableBackwardIterator;
 
-  // 定义一个MemTable用来存储元素的skiplist类型，该skiplist内部的key类型是字符串，
-  // 所用的key比较器类型是KeyComparator
-  typedef SkipList<const char*, KeyComparator> Table;
+} // namespace leveldb
 
-
-  // MemTable类实例用来比较内部元素大小的比较器。
-  KeyComparator comparator_;
-
-  // MemTable类实例的内部引用计数器。
-  int refs_;
-
-  // MemTable类实例用来给元素分配内存时使用的内存管理器。
-  Arena arena_;
-
-  // MemTable类实例用来存储元素的表，其内部实现其实是一个跳表，skiplist。
-  Table table_;
-
-  // No copying allowed
-  MemTable(const MemTable&);
-  void operator=(const MemTable&);
-};
-
-}  // namespace leveldb
-
-#endif  // STORAGE_LEVELDB_DB_MEMTABLE_H_
+#endif // STORAGE_LEVELDB_DB_MEMTABLE_H_
